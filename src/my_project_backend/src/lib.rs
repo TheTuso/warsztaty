ac2e299f491ca1dae6e51a69f4f6ac9af58bacb3
Piotr Pasztor <piotrekpasztor@gmail.com>
2024-12-04 09:39:29 +0000

    Add option to save message

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,4 +1,17 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static MSG: RefCell<String> = RefCell::new(String::new());
+}
+
+#[ic_cdk::update]
+fn set_msg(message: String) {
+    MSG.with(|msg| {
+        *msg.borrow_mut() = message
+    })
+}
+
 #[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+fn get_message() -> String {
+    MSG.with(|msg| msg.borrow().clone())
 }
